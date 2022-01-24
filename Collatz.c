bc6b1f2a3a7e1a78873c60bb3b122d3b0089a981
Oleg Kotok <kotok_oleg@ukr.net>
2022-01-24 21:30:45 +0200

    First commit. Direct (not longest) calculation of Collatz sequence. C Style syntax.

---
+++ Collatz.c
@@ -0,0 +1,66 @@
+﻿/*! \file    Collatz.c
+ *  \brief   Simmple and fast solution to find longest Collatz sequence
+ *	\author  Oleg Kotok
+ *	\date    24.01.2022
+ *  \details Program one. Optimise for code simplicity and low memory usage. Implementation in clear C, without ++.
+ *  \example gcc Collatz.c && time ./a.out 13
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*! 
+ *  \brief Get length of Collatz sequence
+ *  \param [in] n Start number of Collatz sequence
+ *  \return length of Collatz sequence for staring point n
+ */
+int Collatz(unsigned long long n)
+{
+    int i = 1;
+    while (n > 1)
+    {
+        if (n % 2 == 0) /* n is even */
+        {
+            n >>= 1; /* n = n / 2 */
+        }
+        else  /* n is odd number */
+        {
+            n *= 3; /* n = n * 3 */
+            n++;
+        }
+        i++;
+    }
+    return i;
+}
+
+/*! 
+ *  \brief main function
+ *  \param [in] argc {An integer that contains the count of arguments that follow in argv. The argc parameter is always greater than or equal to 1.}
+ *  \param [in] argv {An array of null-terminated strings representing command-line arguments entered by the user of the program.}
+ *  \details argv[1] have to contain ineger value - startiong position of Collatz sequence
+ *  \return Return 0 if everything is ok
+ *          -1 - invalid argument
+ *          -2 - no arguments
+ */
+int main(int argc, char* argv[])
+{
+    if (argc > 1)
+    {
+        int n = atoi(argv[1]);
+        if (n > 0) /* argv[1] is integer */
+        {
+            printf("Iterations: %u \n", Collatz(n));
+            return 0;
+        }
+        else
+        {
+             printf("%s is Invalid argument. Syntax: %s n - starting value of Collatz sequence \n", argv[1], argv[0]);
+            return -1;
+        }
+    }
+    else
+    {
+         printf("Syntax: %s n - starting value of Collatz sequence \n", argv[0]);
+         return -2;
+    }
+}
