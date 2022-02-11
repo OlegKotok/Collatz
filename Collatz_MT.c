814cdb3bec0c6f3a058f10d2e94265048dca1f98
Oleg Kotok <kotok_oleg@ukr.net>
2022-02-11 23:54:28 +0200

    Multithreaded optimization with posix thread

---
+++ Collatz_MT.c
@@ -0,0 +1,133 @@
+﻿/*! \file    Collatz.c
+ *  \brief   Simmple and fast solution to find longest Collatz sequence
+ *  \author  Oleg Kotok
+ *  \date    11.02.2022
+ *  \version v_2
+ *  \details Multithreaded optimization with posix threads
+ *  \example make test-multithreaded TEST_VALUE=10000000 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#define THREAD_COUNT 4
+
+pthread_mutex_t mutex;
+
+/*! 
+ *  \brief Get length of Collatz sequence
+ *  \param [in] n Start number of Collatz sequence
+ *  \return length of Collatz sequence for staring point n
+ */
+int Collatz(unsigned long long n)
+{
+    int i = 0;
+    while (n > 1)
+    {
+        if (n % 2 == 0) /* n is even */
+        {
+            n >>= 1; /* n = n / 2 */
+        }
+        else  /* n is odd number */
+        {
+            n *= 3; /* n = n * 3 */
+            n++;
+        }
+        i++;
+    }
+    return i;
+}
+
+/** Struct to store parameters for thread function. Posix thread doesn't support multiple parameters in thread functions*/
+struct Results
+{
+    int currentstartPosition;           /** Current start number of Collatz sequence. Value will be updated after calculation */
+    int maxLength;                      /** Max length, this value will be updated if this Collatz sequence logest */
+    int startingPositionForMaxSequence; /** Staring position for max length sequence. Ref value */
+};
+
+/*! 
+ *  \brief Get length of Collatz sequence and update max length
+ *  \param args Pointer to Results struct which store parameters for thread function
+ *  \see Results
+ *  \return 0
+ */
+void* updateMaxCollatzSequence(void *args)
+{
+    struct Results *p = (struct Results*) args;
+    while ( p->currentstartPosition > 0 )
+    {
+        pthread_mutex_lock(&mutex);
+        const int startPosition = p->currentstartPosition--;
+        pthread_mutex_unlock(&mutex);
+
+        int currentLength = Collatz(startPosition);
+
+        if ( currentLength > p->maxLength )
+        {
+            pthread_mutex_lock(&mutex);
+            p->maxLength = currentLength;
+            p->startingPositionForMaxSequence = startPosition;
+
+            printf("Basic number: %u \t Sequence length: %u \n", p->startingPositionForMaxSequence, p->maxLength);
+            pthread_mutex_unlock(&mutex);
+
+        }
+    }
+    return 0;
+}
+
+/*! 
+ *  \brief main function
+ *  \param [in] argc {An integer that contains the count of arguments that follow in argv. The argc parameter is always greater than or equal to 1.}
+ *  \param [in] argv {An array of null-terminated strings representing command-line arguments entered by the user of the program.}
+ *  \details argv[1] have to contain ineger value - startiong position of Collatz sequence
+ *  \return Return 0 if everything is ok
+ *          -1 - invalid argument
+ *          -2 - no arguments
+ */
+int main(int argc, char* argv[])
+{
+    printf("C-style multi-threaded optimization\n\n");
+    
+    if (argc == 1)
+    {
+        printf("Syntax: %s n - starting value of Collatz sequence \n", argv[0]);
+        return -2;
+    }
+   
+    int basicSequence = atoi(argv[1]);
+    if (basicSequence == 0) /* argv[1] is not integer */
+    {
+        printf("%s is Invalid argument. Syntax: %s n - starting value of Collatz sequence \n", argv[1], argv[0]);
+        return -1;
+    }
+
+    /** struct to store parameters for thread function */
+    struct Results s = {.maxLength = 0, .startingPositionForMaxSequence = 0, .currentstartPosition = basicSequence};
+
+    pthread_t threads[THREAD_COUNT];
+    pthread_mutex_init(&mutex, NULL);
+    for (int i = 0; i < THREAD_COUNT; i++)
+    {
+        int status = pthread_create(&threads[i], NULL, updateMaxCollatzSequence, (void*) &s);
+        if (status != 0)
+        {
+            printf("main error: can't create thread, status = %d\n", status);
+            exit(status);
+        }
+    }
+    for (int i = 0; i < THREAD_COUNT; i++)
+    {
+        int status = pthread_join(threads[i], NULL);
+        if (status != 0)
+        {
+            printf("main error: can't join thread, status = %d\n", status);
+            exit(status);
+        }
+    }
+    pthread_mutex_destroy(&mutex);
+
+    printf("\nBasic number: %u \t Sequence length: %u \n", s.startingPositionForMaxSequence, s.maxLength);
+    return 0;
+}
--- Makefile
+++ Makefile
@@ -2,29 +2,43 @@ COMPILER=g++
 INCLUDE=thread-pool
 C_OUTPUT=Collatz1
 CPP_OUTPUT=Collatz2
-MULTITHREADED_OUTPUT=Collatz3
+MULTITHREADED_C_OUTPUT=Collatz3
+MULTITHREADED_CPP_OUTPUT=Collatz4
 TEST_VALUE=10000000
 
-all: normal optimized multithreaded
+all: normal multithreaded optimized optimized_mt 
 
 normal:
 	gcc -O3 -Wall Collatz.c -o $(C_OUTPUT)
 
+multithreaded:
+	gcc -O3 -Wall Collatz_MT.c -o $(MULTITHREADED_C_OUTPUT) -lpthread
+
 optimized:
 	$(COMPILER) -std=c++2a -O3 -Wall Collatz.cpp -o $(CPP_OUTPUT)
 
-multithreaded:
-	$(COMPILER) -std=c++2a -O3 -Wall -I$(INCLUDE) Collatz_MT.cpp -o $(MULTITHREADED_OUTPUT) -lpthread
+optimized_mt:
+	$(COMPILER) -std=c++2a -O3 -Wall -I$(INCLUDE) Collatz_MT.cpp -o $(MULTITHREADED_CPP_OUTPUT) -lpthread
 
 clean:
 	rm $(C_OUTPUT)
 	rm $(CPP_OUTPUT)
-	rm $(MULTITHREADED_OUTPUT)
+	rm $(MULTITHREADED_C_OUTPUT)
+	rm $(MULTITHREADED_CPP_OUTPUT)
 
-test:
+test-normal:
 	time ./$(C_OUTPUT) $(TEST_VALUE)
+
+test-cpp:
 	time ./$(CPP_OUTPUT) $(TEST_VALUE)
-	time ./$(MULTITHREADED_OUTPUT) $(TEST_VALUE)
+
+test-multithreaded:
+	time ./$(MULTITHREADED_C_OUTPUT) $(TEST_VALUE)
+
+test-multithreaded-cpp:
+	time ./$(MULTITHREADED_CPP_OUTPUT) $(TEST_VALUE)
+
+test: test-normal  test-multithreaded test-cpp test-multithreaded-cpp
 
 docs:
 	doxygen doxygen.conf
