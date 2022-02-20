808759449242b8cbeb0608f3b289c48c8785c377
Oleg Kotok <kotok_oleg@ukr.net>
2022-02-20 09:12:54 +0200

    Multithreading support

--- Collatz.go
+++ Collatz.go
@@ -1,27 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"os"
-	"strconv"
+    "fmt"
+    "os"
+    "strconv"
 )
 
+const thread_limit = 1
+
 var (
-	maxLength int = 0
-	startingPositionForMaxSequence  uint64 = 0;
+    maxLength int = 0
+    startingPositionForMaxSequence  uint64 = 0;
 )
 
 func Collatz(n uint64) {
-	var length int
-	var startPosition = n
-	for ; n > 1; length++ {
-		if n % 2 == 0 /* n is even */ {
+    var length int
+    var startPosition = n
+    for ; n > 1; length++ {
+        if n % 2 == 0 /* n is even */ {
             n >>= 1; /* n = n / 2 */
         } else  /* n is odd number */ {
             n *= 3; /* n = n * 3 */
             n++;
         }
-	}
+    }
     
     if length > maxLength {
         maxLength = length;
@@ -32,20 +34,25 @@ func Collatz(n uint64) {
 }
 
 func main() {
-	fmt.Println("Go solution\n")
-	
-	if len (os.Args) == 1 {
+    fmt.Println("Go solution\n")
+    
+    if len (os.Args) == 1 {
         fmt.Println("Syntax: ", os.Args[0], "n - starting value of Collatz sequence \n")
         os.Exit(-2)
     }
 
-	var basicSequence, err = strconv.Atoi (os.Args[1])
+    var basicSequence, err = strconv.Atoi (os.Args[1])
     if err != nil /* argv[1] is not integer */ {
         fmt.Println( os.Args[1], " is Invalid argument. Syntax: ", os.Args[0], " n - starting value of Collatz sequence \n" )
         os.Exit(-1)
     }
 
-	for ; basicSequence > 0; basicSequence-- {
-		Collatz (uint64 (basicSequence) )
-	}
+    for i := 1; basicSequence > 0; basicSequence-- {
+        if i % thread_limit == 0 {
+            Collatz (uint64 (basicSequence) )
+        } else {
+            go Collatz (uint64 (basicSequence) )    
+        }
+        i++
+    }
 }
